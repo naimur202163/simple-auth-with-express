74eab6b04aefd34633e8c8db6e73b900a2af834e
naimur <naimur202163@gmail.com>
2022-07-24 14:10:19 +0600

    routes are added

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
-
+const User=require('./model/user');
 const app = express();
 
 // Middleware
-
 app.use(express.json());
+
+
+
+// Registar
+
+app.post('/registar',(req,res)=>{
+
+})
+
+
+
+// login
+app.post('/login',(req,res)=>{
+    
+})
+
+
+
 
 module.exports = app;
