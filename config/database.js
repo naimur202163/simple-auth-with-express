9961d1738bfd1c8bdbacd20ba60447fc59898f1e
naimur <naimur202163@gmail.com>
2022-07-24 14:05:52 +0600

    userSchema added

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = () => {
   mongoose
-    .connect(`mongodb://localhost:27017`, {
+    .connect(`mongodb://localhost:27017/authinticaion`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
--- model/user.js
+++ model/user.js
@@ -1,1 +1,25 @@
-const mongoose =require('mongoose');
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    default: none,
+  },
+  last_name: {
+    type: String,
+    default: none,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+});
+
+
+module.exports=mongoose.model("User",userSchema);
