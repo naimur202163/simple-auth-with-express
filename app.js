0f09db4cc050b225d46c1208b0bf4774b19c27d7
naimur <naimur202163@gmail.com>
2022-07-24 14:38:00 +0600

    user registation form added

--- app.js
+++ app.js
@@ -1,26 +1,61 @@
 const express = require("express");
-const User=require('./model/user');
+const User = require("./model/user");
 const app = express();
 
 // Middleware
 app.use(express.json());
 
-
-
 // Registar
 
-app.post('/registar',(req,res)=>{
+app.post("/register", async (req, res) => {
+  // Our register logic starts here
+  try {
+    // Get user input
+    const { first_name, last_name, email, password } = req.body;
 
-})
+    // Validate user input
+    if (!(email && password && first_name && last_name)) {
+      res.status(400).send("All input is required");
+    }
 
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
 
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
 
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
 // login
-app.post('/login',(req,res)=>{
-    
-})
-
-
-
+app.post("/login", (req, res) => {});
 
 module.exports = app;
